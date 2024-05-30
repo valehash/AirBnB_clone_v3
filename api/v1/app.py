5460561e774dcceb52db71a1521ae98c59702825
vale-hash <newlechim@gmail.com>
2024-05-30 11:35:47 +0100

    airbnbn clonev3 status response 200 removed quotes on ports

--- api/v1/app.py
+++ api/v1/app.py
@@ -15,4 +15,4 @@ def teardown_appcontext(exception):
     storage.close()
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('HBNB_API_HOST', '0.0.0.0'), port=(int(os.getenv('HBNB_API_PORT','5000'))), threaded=True)
+    app.run(host=os.getenv('HBNB_API_HOST', 0.0.0.0), port=(int(os.getenv('HBNB_API_PORT',5000))), threaded=True)
